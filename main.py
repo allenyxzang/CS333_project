6c54ac4497a86431678c277a1042527c03eb0214
Alex Kolar <alexkolar@comcast.net>
2021-12-01 11:49:37 -0600

    Move request positioning in loop

--- main.py
+++ main.py
@@ -40,8 +40,13 @@ def run_simulation(graph_arr, nodes, request_stack, end_time):
     requests_to_serve = []  # keep track of incomplete requests, in case new request comes in before previous request is completed
     entanglement_available = []  # keep track of entanglement links from route nodes when a request is submitted
     entanglement_ondemand = []  # keep track of entanglement links generated on demand to complete a request
-    # get first request
-    request = request_stack.pop(0)
+
+    # track current request and related info
+    next_request_to_submit = request_stack.pop(0)
+    current_request = None
+    origin_node = None
+    destination_node = None
+    route = []
 
     while time < end_time:
         # check if memories expired
@@ -52,25 +57,32 @@ def run_simulation(graph_arr, nodes, request_stack, end_time):
                     node.memo_expire(memory)
 
         # determine if a new request is submitted to the network
-        if time == request.submit_time:
+        if time == next_request_to_submit.submit_time:
             # submit request
-            requests_to_serve.append(request)
-            
+            requests_to_serve.append(next_request_to_submit)
+
             # find path and assign to route attribute
-            route = request.get_path(graph_arr, nodes)
-            request.route = route
+            new_route = next_request_to_submit.get_path(graph_arr, nodes)
+            next_request_to_submit.route = new_route
+
+            # assign as current request if there is none
+            if current_request is None:
+                current_request = next_request_to_submit
+                route = new_route
+                origin_node = nodes[route[0]]
+                destination_node = nodes[route[-1]]
 
             # get new request
             if len(request_stack) > 0:
-                request = request_stack.pop(0)
+                next_request_to_submit = request_stack.pop(0)
 
             # update node information on other nodes in path
             # adaptively update probability distribution when a request is submitted to the network
-            for i, label in enumerate(route):
+            for i, label in enumerate(new_route):
                 node = nodes[label]
 
-                left_neighbors_to_connect = route[0:i]
-                right_neighbors_to_connect = route[i:-1]
+                left_neighbors_to_connect = new_route[:i]
+                right_neighbors_to_connect = new_route[i+1:]
                 nodes[label].left_neighbors_to_connect.append(left_neighbors_to_connect)
                 nodes[label].right_neighbors_to_connect.append(right_neighbors_to_connect)
 
@@ -87,25 +99,15 @@ def run_simulation(graph_arr, nodes, request_stack, end_time):
                         entanglement_available.extend(links)
                 # get links used for request
                 if i > 0:
-                    links_used.append(route[i-1])
-                if i < (len(route) - 1):
-                    links_used.append(route[i+1])
+                    links_used.append(new_route[i-1])
+                if i < (len(new_route) - 1):
+                    links_used.append(new_route[i+1])
 
                 # record entanglement links available (stemming from nodes in route) and reset entanglement_available
                 entanglement_usage_pattern["available"].append(entanglement_available)
                 entanglement_available = []
 
                 node.generation_protocol.update_dist(links_available, links_used)
-        
-        # serve only the first request in queue at a time
-        if len(requests_to_serve) > 0:
-            route = requests_to_serve[0].route
-            origin_node = nodes[route[0]]
-            destination_node = nodes[route[-1]]
-        else:
-            origin_node = None
-            destination_node = None
-            route = []
 
         # call function to run node (entanglement generation) protocol
         for node in nodes:
@@ -196,14 +198,13 @@ def run_simulation(graph_arr, nodes, request_stack, end_time):
                 node.create_random_link(time)
 
         # determine if the desired entanglement is established
-        if len(requests_to_serve) > 0:
+        if current_request is not None:
             for memory in origin_node.memories:
-                # check if have memory entangled with destination
+                # check if we have memory entangled with destination
                 if memory.entangled_memory["node"] == destination_node:
                     # record latency and completion time
-                    completed_request = requests_to_serve.pop(0)
-                    latency = time - completed_request.submit_time
-                    serve_time = time - completed_request.start_time
+                    latency = time - current_request.submit_time
+                    serve_time = time - current_request.start_time
                     latencies.append(latency)
                     serve_times.append(serve_time)
                     request_complete_times.append(time)
@@ -218,10 +219,19 @@ def run_simulation(graph_arr, nodes, request_stack, end_time):
                     # expire memories
                     origin_node.memo_expire(memory)
 
+                    requests_to_serve.pop(0)
                     # if waiting on any requests to serve, they will start at next time step
                     if len(requests_to_serve) > 0:
-                        next_request = requests_to_serve[0]
-                        next_request.start_time = time + 1
+                        current_request = requests_to_serve[0]
+                        current_request.start_time = time + 1
+                        route = current_request.route
+                        origin_node = nodes[route[0]]
+                        destination_node = nodes[route[-1]]
+                    else:
+                        current_request = None
+                        route = []
+                        origin_node = None
+                        destination_node = None
 
                     break
 
